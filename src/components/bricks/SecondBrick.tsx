5817151cd71ee95d65d87d1e1f1fccd29a4aacac
Soufiane Aït Ouarraou <soufiane.ait@outlook.fr>
2023-07-15 13:53:25 +0200

    axios hook and countdown setup

--- src/components/bricks/SecondBrick.tsx
+++ src/components/bricks/SecondBrick.tsx
@@ -1,16 +1,70 @@
-import React from "react";
 import { Brick } from "../Brick";
-import { Button, Card } from "react-bootstrap";
+import Countdown from "react-countdown";
+import { Datum } from "../Datum";
+
+const Completionist = () => <span>Lancement imminent !</span>;
+
+type RendererProps = {
+  days: number,
+  hours: number,
+  minutes: number,
+  seconds: number,
+  completed: boolean
+}
+
+
+const nextLaunch = 1690081440000
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+  hour: "numeric",
+  minute: "numeric"
+};
+const frenchDate = new Date(nextLaunch).toLocaleString("fr-FR", options);
 
 export const SecondBrick = () => {
+  // const { response, error, loading } = useAxios('/v4/launches/next')
+
+
+  const renderer = ({ days, hours, minutes, seconds, completed }: RendererProps) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return (
+        <div className="d-flex gap-3">
+          <div className="box">
+            <Datum datum={{title: 'Jours', datum: days}}></Datum>
+          </div>
+          <div className="box">
+            <Datum datum={{title: 'Heures', datum: hours}}></Datum>
+          </div>
+          <div className="box">
+            <Datum datum={{title: 'Minutes', datum: minutes}}></Datum>
+          </div>
+          <div className="box">
+            <Datum datum={{title: 'Secondes', datum: seconds}}></Datum>
+          </div>
+        </div>
+      );
+    }
+  };
   return (
     <Brick style={{ maxHeight: "285px" }}>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
+      <div className="box p-2">
+        <p className="box-title">Prochain lancement</p>
+        <div className="box-body p-2 text-center">
+          <div className="box p-2">
+            <div className="box-title text-start">Contenu</div>
+            <div className="box-body p-2">fusee + capsule</div>
+          </div>
+          <small className="text-center">{frenchDate} (heure de Paris)</small>
+          <Countdown date={nextLaunch} renderer={renderer} className=""/>
+        </div>
+      </div>
     </Brick>
   );
 };
---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.spacexdata.com';
+
+const useAxios = (endpoint: string) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setloading] = useState(true);
+
+    const fetchData = () => {
+        axios
+            .get(endpoint)
+            .then((res) => {
+                setResponse(res.data);
+            })
+            .catch((err) => {
+                setError(err);
+            })
+            .finally(() => {
+                setloading(false);
+            });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // custom hook returns value
+    return { response, error, loading };
+};
+
+export default useAxios;
