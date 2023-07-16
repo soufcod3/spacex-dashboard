4581eefbb25f964dc3a8d5500370e9659f4f8f98
Soufiane Aït Ouarraou <soufiane.ait@outlook.fr>
2023-07-16 12:38:49 +0200

    removing console log

--- src/components/bricks/FifthBrick.tsx
+++ src/components/bricks/FifthBrick.tsx
@@ -4,15 +4,14 @@ import { News } from "../News";
 
 export const FifthBrick = () => {
   const { response: allNews, error, loading } = useAxios("/v4/history");
-console.log(allNews)
   return (
     <Brick mb={0} style={{ height: "300px" }}>
       <div className="box p-2">
         <div className="box-title">Bon à savoir</div>
         <div className="box-body py-2 overflow-y-scroll" style={{ height: "250px" }}>
           {
-            allNews && allNews.map((news) => {
-          return <News news={news} />
+            allNews && allNews.map((news, idx) => {
+          return <News news={news} key={} />
             })
           }
         </div>
