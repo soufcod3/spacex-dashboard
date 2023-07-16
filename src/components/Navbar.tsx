0bcc959a507fb29305cf01624ba37c92114014ae
Soufiane Aït Ouarraou <soufiane.ait@outlook.fr>
2023-07-16 18:28:34 +0200

    navbar quick fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,54 @@
-import React from 'react'
-import spaceXLogo from '../assets/spacex-logo.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import spaceXLogo from "../assets/spacex-logo.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { GrDocumentDownload } from "react-icons/gr";
 
 export const Navbar = () => {
-  return <nav className='py-3'>
-    <div className="credit position-absolute">
-      <img src={spaceXLogo} alt="Logo de Space X" style={{ width: '8rem' }} className='mb-1'/>
-      <p>présenté par</p>
-      <small>SOUFIANE AÏT OUARRAOU</small>
-    </div>
-    <div className="navigation">
-      <div className="nav-item"></div>
-      <div className="nav-item"></div>
-      <div className="nav-item"></div>
-    </div>
-  </nav>
-}
+  return (
+    <nav className="py-3">
+      <div className="credit position-absolute d-none d-sm-block">
+        <img
+          src={spaceXLogo}
+          alt="Logo de Space X"
+          style={{ width: "8rem" }}
+          className="mb-1"
+        />{" "}
+        <br />
+        <small className="text-secondary">
+          présenté par SOUFIANE AÏT OUARRAOU
+        </small>
+      </div>
+      <div className="navigation">
+        <div className="nav-item d-flex justify-content-center align-items-center">
+          <a
+            href="https://github.com/soufcod3/spacex-dashboard"
+            className="text-dark"
+            target="blank"
+          >
+            <h2>
+              <FaGithub />
+            </h2>
+          </a>
+        </div>
+        <div className="nav-item d-flex justify-content-center align-items-center">
+          <a
+            href="https://linkedin.com/in/soufiane-ait"
+            className="text-dark"
+            target="blank"
+          >
+            <h2>
+              <FaLinkedin />
+            </h2>
+          </a>
+        </div>
+        <div className="nav-item d-flex justify-content-center align-items-center">
+          <a href="https://purple-karlen-12.tiiny.site/" target="blank">
+            <h2>
+              <GrDocumentDownload />
+            </h2>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
